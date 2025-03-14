4b53164bbee9ca98a7dbe0025a61e07052d4ea5d
Ruben Torres Frias <isctorres@gmail.com>
2025-03-14 11:19:54 -0600

    hilos

--- src/main/java/com/example/tap2025/vistas/Celayork.java
+++ src/main/java/com/example/tap2025/vistas/Celayork.java
@@ -1,5 +1,6 @@
 package com.example.tap2025.vistas;
 
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
@@ -14,10 +15,21 @@ public class Celayork extends Stage {
     private Button btnIniciar;
     private Label[] lblRutas;
     private ProgressBar[] pgbRutas;
+    private Scene escena;
 
     public  Celayork(){
+        CrearUI();
         this.setTitle("Calles de Celaya");
         this.setScene(escena);
         this.show();
+    }
+
+    private void CrearUI() {
+        pgbRutas = new ProgressBar[5];
+        lblRutas = new Label[5];
+        gdpCalles = new GridPane();
+        for (int i = 0; i < pgbRutas.length; i++) {
+
+        }
     }
 }
